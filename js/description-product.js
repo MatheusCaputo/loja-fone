8e93a3e4d7e3ef53dae348b2e145314066f845e5
Matheus Caputo <matheuscaputo17@gmail.com>
2020-01-22 11:20:38 -0300

    Adicionado ouvinte de click para o botao add e inserindo HTML via js (Desafio basico, intermediario e avancado-> aula 13)

--- js/description-product.js
+++ js/description-product.js
@@ -1,7 +1,14 @@
 const $like = window.document.querySelector(".-like");
+const $btnAdd = window.document.querySelector(".-second");
+const $cart = window.document.querySelector(".-last");
 
-$like = document.addEventListener("click", handleClick);
+$like.addEventListener("click", handleClick);
+$btnAdd.addEventListener("click", handleAdd);
 
 function handleClick() {
   console.log("Cliquei no coracao");
+}
+
+function handleAdd() {
+  $cart.innerHTML = "Foi";
 }
